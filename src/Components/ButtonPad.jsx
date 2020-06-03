ffa403a9220c25edeb048ceadeb6b43b149d3e75
Monica Howard <mo@Monicas-Air.attlocal.net>
2020-06-03 12:42:51 -0500

    creates increment, decrement, and reset functions & displays count

--- src/Components/ButtonPad.jsx
+++ src/Components/ButtonPad.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function ButtonPad() {
+    
+    const [count, setCount] = useState(0);
+
+    const decrementClick = () => {
+        setCount(count - 1)
+    }
+
+    const incrementClick = () => {
+        setCount(count + 1);
+    }
+
+    const resetClicks = () => {
+        setCount(0);
+    }
+
     return (
         <div>
-            <button>Up Button</button>
-            <button>Down Button</button>
+            {count}
+            <button onClick={decrementClick}>click</button>
+            <button onClick={incrementClick} >click</button>
+            <button onClick={resetClicks}>Reset</button>
         </div>
     )
 }
