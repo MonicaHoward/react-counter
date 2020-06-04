4c4a4a7d8b8b7fdde2f642ba19cd5a6d549ef5d4
Monica Howard <mo@Monicas-Air.attlocal.net>
2020-06-03 19:44:32 -0500

    adds animation and updates some styling

--- src/Components/ButtonPad.jsx
+++ src/Components/ButtonPad.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react'
+import { AnimateOnChange } from 'react-animation'
+
+
+
 
 export default function ButtonPad() {
     
     const [count, setCount] = useState(0);
+    const [visible, setVisible] = useState(true);
+    const [animate, setAnimation] = useState(false)
 
     const decrementClick = () => {
         setCount(count - 1)
@@ -10,7 +16,12 @@ export default function ButtonPad() {
 
     const incrementClick = () => {
         setCount(count + 1);
+        setAnimation(true);
+        setVisible(false);
+
+      
     }
+
 
     const resetClicks = () => {
         window.confirm("Are you sure you want to start over? Press 'OK' to reset and 'Cancel' to go back") ? setCount(0) : setCount(count)
@@ -24,10 +35,17 @@ export default function ButtonPad() {
             >
                 - 1
             </button>
-            <span className="main count">{count}</span>
+            <span className="main count">
+            <AnimateOnChange animation="bounceIn">
+
+            <h1>
+                {count}</h1 > 
+            </AnimateOnChange>
+            </span>
             <button 
                 className="main inc" 
                 onClick={incrementClick} 
+               
             >
                 + 1
             </button>
